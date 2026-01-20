032a87a98c31e5df2e71c94e4d067c9c1f6e4259
ATTA <attamohamed582@gmail.com>
2026-01-20 22:24:24 +0200

    Home Screen & Brand Scroller Animation

--- lib/main.dart
+++ lib/main.dart
@@ -55,4 +55,79 @@ class _BuildHubAppState extends State<BuildHubApp> {
     );
   }
 }
+
+class HomeScreen extends StatefulWidget {
+  final Function(ThemeMode) onThemeChanged;
+  final ThemeMode currentTheme;
+
+  const HomeScreen({
+    Key? key,
+    required this.onThemeChanged,
+    required this.currentTheme,
+  }) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
 }
+
+class _HomeScreenState extends State<HomeScreen> {
+  late ScrollController _scrollController;
+  late Timer _scrollTimer;
+  final double _scrollSpeed = 1.0;
+
+  final List<String> _brandImages = [
+    'assets/images/compumarts.png',
+    'assets/images/noor-tech.png',
+    'assets/images/sigma.png',
+    'assets/images/el-nekhely.png',
+    'assets/images/elbadr.png',
+    'assets/images/yamama.png',
+    'assets/images/alfransia.png',
+    'assets/images/hardware-market.png',
+  ];
+
+  final Set<int> _selectedBrands = {};
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController = ScrollController();
+    _startAutoScroll();
+  }
+
+  void _startAutoScroll() {
+    _scrollTimer = Timer.periodic(const Duration(milliseconds: 50), (timer) {
+      if (_scrollController.hasClients) {
+        final maxScroll = _scrollController.position.maxScrollExtent;
+        final currentScroll = _scrollController.offset;
+
+        if (currentScroll >= maxScroll) {
+          _scrollController.jumpTo(0);
+        } else {
+          _scrollController.animateTo(
+            currentScroll + _scrollSpeed,
+            duration: const Duration(milliseconds: 50),
+            curve: Curves.linear,
+          );
+        }
+      }
+    });
+  }
+
+  Future<void> _openWebsite() async {
+    final Uri url = Uri.parse('https://www.buildhub.studio/en');
+    if (await canLaunchUrl(url)) {
+      await launchUrl(url);
+    } else {
+      debugPrint('Could not launch website');
+    }
+  }
+
+  @override
+  void dispose() {
+    _scrollTimer.cancel();
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  
